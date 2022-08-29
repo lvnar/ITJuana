4646060e5a42f9839551cfc2e8242858f5daca0e
César Pérez Canseco <perez.canseco@gmail.com>
2022-08-29 17:36:38 -0500

    Numbers base conversion

---
+++ BaseConversion.py
@@ -0,0 +1,15 @@
+# Numbers base conversion
+# Write a function that takes two integers (x and b) as inputs
+# and returns a string that represents the number x in base b 
+
+def baseConversion(a, b):
+    if (a // b) == 0:
+        return str(a)
+    return baseConversion(a // b, b) + str(a % b)
+
+
+# Examples:
+# If x=5 and b=2 then the function will return "101" 
+print(baseConversion(5, 2))
+# If x=5 and b=3 then the function will return "12" 
+print(baseConversion(5, 3))
